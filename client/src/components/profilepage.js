5cd76cf7ad490155a92e0e0edfa123eccebd26ec
Brian Oblad <a.gray.cloud@gmail.com>
2019-10-09 12:57:19 -0400

    start doing apollo things

--- client/src/components/profilepage.js
+++ client/src/components/profilepage.js
@@ -1,15 +1,27 @@
-import React from "react";
-import ColumnData from "./profilecolumn";
+import React from 'react';
+import ColumnData from './profilecolumn';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_POSTS = gql`
+  {
+    communities {
+      name
+      category
+    }
+  }
+`;
 
 export default class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      owned: ["Cats", "Dog People"],
-      belongTo: ["Cat People", "People who like umbrellas"],
-      public: ["I need to buy cups", "Computers"]
+      owned: ['Cats', 'Dog People'],
+      belongTo: ['Cat People', 'People who like umbrellas'],
+      public: ['I need to buy cups', 'Computers'],
     };
   }
+
   render() {
     return (
       <div className="profile-container">
@@ -21,10 +33,18 @@ export default class UserProfile extends React.Component {
           headerText="Communities You're Subscribed To"
           listData={this.state.belongTo}
         />
-        <ColumnData
-          headerText="Public Communities"
-          listData={this.state.public}
-        />
+        <Query query={GET_POSTS}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Loading</div>;
+            if (error) console.log(error);
+            return (
+              <ColumnData
+                headerText="Public Communities"
+                listData={this.state.public}
+              />
+            );
+          }}
+        </Query>
       </div>
     );
   }
