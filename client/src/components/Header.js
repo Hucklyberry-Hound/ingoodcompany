08b3e4bc49c4ab9688029faf46a3708528f0d383
Andrew O'Grady <andrewogrady3@gmail.com>
2019-10-13 09:48:05 -0500

    more style;

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,18 +5,34 @@ import { AUTH_TOKEN, USER } from '../constants';
 import '../styles/Header.css'
 
 class Header extends React.Component {
-  
+  constructor(){
+    super()
+    this.state = {
+      show: false
+    }
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  toggleMenu() {
+    this.setState({show: !this.state.show})
+    const menu = document.getElementsByClassName("menu-burger")[0].classList.toggle('active-burger');
+  }
+
   render() {
     const authToken = localStorage.getItem(AUTH_TOKEN);
     const user = localStorage.getItem(USER);
     return (
       <div className ="header-bar">
-        <div >
+        <div>
           {authToken ? (
             <div className="loggedin-header-bar">
+              <div className="menu-burger">
+              <i class="fa fa-bars" aria-hidden="true" onClick={() => this.toggleMenu()}></i>
+              </div>
+              {(this.state.show) ?
+              <React.Fragment>
               <h4 className="welcome-header-bar">Welcome {user}!</h4>
               <div className="links-header-bar">
-
               <Link className="element-header-bar" to="/home">
               <div >home</div>
               </Link>
@@ -43,6 +59,7 @@ class Header extends React.Component {
               </Link> 
 
               </div>
+              </React.Fragment> : ""}
             </div>
           ) : (
             <Link to="/login"> login/create account </Link>
