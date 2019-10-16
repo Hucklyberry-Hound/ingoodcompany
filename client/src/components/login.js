94632f49473440769a151145cd68a46597456a90
Andrew O'Grady <andrewogrady3@gmail.com>
2019-10-16 16:03:49 -0400

    fixed default image

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -43,14 +43,6 @@ const LOGIN_MUTATION = gql`
 `;
 
 
-//generate default pic
-const randIdx =  Math.floor(Math.random() * 3)
-const defaultImages = [ 
-    'https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Alien-512.png',
-    'https://66.media.tumblr.com/15aa93b2b3b4949d6d061735c9ba6b21/tumblr_inline_n6y3qnRoSg1r73jj6.png',
-    'https://cdn.imgbin.com/0/10/15/imgbin-robot-scalable-graphics-euclidean-icon-robot-qDGV4CMnQsejEwaqGRvRiU1PH.jpg',
-    'https://cdn.dribbble.com/users/2101624/screenshots/6068793/dribbble5.jpg'
-]
 
 class Login extends Component {
   constructor() {
@@ -61,7 +53,7 @@ class Login extends Component {
       password: '',
       firstName: '',
       lastName: '',
-      image: '' || defaultImages[randIdx],
+      image: '',
       username: '',
     };
 
